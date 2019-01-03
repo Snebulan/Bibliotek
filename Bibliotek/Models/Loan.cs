1c16edf36c24abbf6820428fa445f3155dfef33b
Tommy Dufmats <tommy.dufmats@gmail.com>
2019-01-03 14:19:10 +0100

    Added loan

---
+++ Bibliotek/Models/Loan.cs
@@ -0,0 +1,18 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bibliotek.Models
+{
+    public class Loan
+    {
+        [Key]
+        public int ID { get; set; }
+        public BookCopy Book { get; set; }
+        [Display(Name = "Medlemsnummer")]
+        public int MemberID { get; set; }
+    }
+}
