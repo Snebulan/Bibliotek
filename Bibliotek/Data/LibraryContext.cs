e43a22d9185482cad63c233f9edf05696134f0a8
Fredrik Gustasfsson <fredrik@vagel.se>
2019-01-03 16:46:45 +0100

    members

--- Bibliotek/Data/LibraryContext.cs
+++ Bibliotek/Data/LibraryContext.cs
@@ -2,11 +2,6 @@
 using Bibliotek.Models.ViewModels;
 using Library.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Bibliotek.Models.ViewModels;
 
 namespace Library.Data
 {
---
+++ Bibliotek/Migrations/20190103151628_sixth.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bibliotek.Migrations
+{
+    public partial class sixth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LoanID",
+                table: "Members");
+
+            migrationBuilder.InsertData(
+                table: "Members",
+                columns: new[] { "ID", "FirstName", "LastName" },
+                values: new object[] { 1, "Fredrik", "Gustafsson" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Members",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LoanID",
+                table: "Members",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
