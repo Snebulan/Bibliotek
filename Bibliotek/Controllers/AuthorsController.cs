0218a3fe720862de5c4427f7b9a5b104b1566c4a
Sebastian <snebulan@yahoo.com>
2019-01-03 14:23:49 +0100

    Created AuthorsController

---
+++ Bibliotek/Controllers/AuthorsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Library.Data;
+using Library.Models;
+
+namespace Bibliotek.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public AuthorsController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Authors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Authors.ToListAsync());
+        }
+
+        // GET: Authors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Authors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,FirstName,LastName")] Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(author);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return _context.Authors.Any(e => e.ID == id);
+        }
+    }
+}
