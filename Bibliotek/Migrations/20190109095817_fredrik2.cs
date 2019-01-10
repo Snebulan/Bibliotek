1506ba1a469134e10af79aa49d9809d2da737b84
Fredrik Gustasfsson <fredrik@vagel.se>
2019-01-10 10:08:28 +0100

    ny sync

---
+++ Bibliotek/Migrations/20190109095817_fredrik2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bibliotek.Migrations
+{
+    public partial class fredrik2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PersonNumber",
+                table: "Members",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PersonNumber",
+                table: "Members");
+        }
+    }
+}
