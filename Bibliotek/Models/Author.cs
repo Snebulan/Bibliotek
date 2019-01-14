8540bdc1bddfaa6833703402920eca9f223eaad7
Fredrik Gustasfsson <fredrik@vagel.se>
2019-01-14 20:20:09 +0100

    utan validering author

--- Bibliotek/Models/Author.cs
+++ Bibliotek/Models/Author.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Bibliotek.Models
 {
-    public class Author
+    public class Author 
     {
         [Key]
         public int ID { get; set; }
 
         [Display(Name = "Förnamn")]
-        [Required(ErrorMessage = "Förnamn måste anges")]
+        //[Required(ErrorMessage = "Förnamn måste anges")]
         public string FirstName { get; set; }
-        [Required(ErrorMessage ="Efternamn måste anges")]
+
         [Display(Name = "Efternamn")]
+        //[Required(ErrorMessage ="Efternamn måste anges")]
+        
         public string LastName { get; set; }
         public virtual ICollection<Book> AuthorBooks { get; set; }
 
@@ -20,6 +23,5 @@ namespace Bibliotek.Models
         {
             get { return FirstName + " " + LastName; }
         }
-
     }
 }
